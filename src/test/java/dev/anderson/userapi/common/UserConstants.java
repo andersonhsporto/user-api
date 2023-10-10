eaf6848292f33fd701e5903f8566e7653e416bbc
Anderson Porto <ahigo@minsait.com>
2023-10-10 10:22:46 -0300

    Add: create unit tests

--- src/test/java/dev/anderson/userapi/common/UserConstants.java
+++ src/test/java/dev/anderson/userapi/common/UserConstants.java
@@ -1,25 +1,33 @@
 package dev.anderson.userapi.common;
 
 import java.time.LocalDate;
-
+import java.time.LocalDateTime;
 import dev.anderson.userapi.users.domain.UserDto;
 import dev.anderson.userapi.users.domain.UserEntity;
 
 public class UserConstants {
-	
-	public static final UserDto VALID_INPUT_DTO = 
-			UserDto.builder()
-			.name("Name")
-			.username("username-dto")
-			.password("very-safe-password")
-			.dateOfBirth(LocalDate.of(1999, 1, 1))
-			.build();
-	
-	public static final UserEntity VALID_INPUT_ENTITY =
-			UserEntity.builder()
-				.name("Valid name")
-				.username("Valid username")
-				.dateOfBirth(LocalDate.of(1999, 1, 1))
-				.build();
+
+  public static final UserDto VALID_INPUT_DTO =
+      UserDto.builder().name("Valid name")
+      .username("Valid username")
+      .password("very-safe-password")
+      .dateOfBirth(LocalDate.of(1999, 1, 1))
+      .build();
+
+  public static final UserDto INVALID_INPUT_DTO = UserDto.builder().name(null).username(null)
+      .password(null).dateOfBirth(null).build();
+
+  public static final UserEntity VALID_INPUT_ENTITY =
+      UserEntity.builder().name("Valid name").username("Valid username")
+          .password("very-safe-password").dateOfBirth(LocalDate.of(1999, 1, 1)).build();
+
+  public static final UserEntity VALID_OUTPUT_ENTITY =
+      UserEntity.builder().name("Valid name").username("Valid username")
+          .password("very-safe-password").dateOfBirth(LocalDate.of(1999, 1, 1))
+          .createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
+  
+  public static final UserEntity INVALID_INPUT_ENTITY =
+      UserEntity.builder().name(null).username(null)
+      .password(null).dateOfBirth(null).build();
 
 }
--- src/test/java/dev/anderson/userapi/users/domain/UserServiceTest.java
+++ src/test/java/dev/anderson/userapi/users/domain/UserServiceTest.java
@@ -1,28 +1,42 @@
 package dev.anderson.userapi.users.domain;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static dev.anderson.userapi.common.UserConstants.INVALID_INPUT_DTO;
+import static dev.anderson.userapi.common.UserConstants.INVALID_INPUT_ENTITY;
 import static dev.anderson.userapi.common.UserConstants.VALID_INPUT_DTO;
-
+import static dev.anderson.userapi.common.UserConstants.VALID_INPUT_ENTITY;
+import static dev.anderson.userapi.common.UserConstants.VALID_OUTPUT_ENTITY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import dev.anderson.userapi.common.UserConstants;
-
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-	
-	@InjectMocks
-	private UserService userService;
-	
-	@Mock
-	private UserRepository userRepository;
 
-//	@Test
-//	void createUser_WithValidData_ReturnsUser() {
-//		when(userRepository.save(VALID_INPUT_DTO))
-//	}
+  @InjectMocks
+  private UserService userService;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Test
+  void createUser_WithValidData_ReturnsUser() {
+    when(userRepository.save(VALID_INPUT_ENTITY)).thenReturn(VALID_OUTPUT_ENTITY);
+		
+	UserDto sut = userService.create(VALID_INPUT_DTO);
+		
+	assertThat(sut.name()).isEqualTo(VALID_INPUT_DTO.name());
+  }
+
+  @Test
+  void createUser_WithValidData_ThrowsException() {
+    when(userRepository.save(INVALID_INPUT_ENTITY)).thenThrow(RuntimeException.class);
+    
+    assertThatThrownBy(() -> userService.create(INVALID_INPUT_DTO)).isInstanceOf(RuntimeException.class);
+  }
 
 }
