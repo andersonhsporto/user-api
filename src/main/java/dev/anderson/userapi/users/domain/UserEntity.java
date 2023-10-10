316a22f189918ebd86723199a03165047ddb32ad
Anderson Porto <anderson.higo2@gmail.com>
2023-10-10 10:22:46 -0300

    Add: create unit tests

--- src/main/java/dev/anderson/userapi/users/domain/UserEntity.java
+++ src/main/java/dev/anderson/userapi/users/domain/UserEntity.java
@@ -2,7 +2,7 @@ package dev.anderson.userapi.users.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+import java.util.Objects;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -35,16 +35,16 @@ public class UserEntity {
 	@Column(name = "id")
 	private Long id;
 
-	@Column(name = "name")
+	@Column(name = "name", nullable = false)
 	private String name;
 	
-	@Column(name = "username", unique = true)
+	@Column(name = "username", unique = true, nullable = false)
 	private String username;
 
-	@Column(name = "password")
+	@Column(name = "password", nullable = false)
 	private String password;
 
-	@Column(name = "date_of_birth")
+	@Column(name = "date_of_birth", nullable = false)
 	private LocalDate dateOfBirth;
 
 	@CreatedDate
@@ -64,5 +64,25 @@ public class UserEntity {
 		return "UserEntity [id = " + id + ", name=" + name + ", username=" + username + ", password=" + password
 				+ ", dateOfBirth=" + dateOfBirth + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
 	}
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(createdAt, dateOfBirth, id, name, password, updatedAt, username);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    UserEntity other = (UserEntity) obj;
+    return Objects.equals(createdAt, other.createdAt)
+        && Objects.equals(dateOfBirth, other.dateOfBirth) && Objects.equals(id, other.id)
+        && Objects.equals(name, other.name) && Objects.equals(password, other.password)
+        && Objects.equals(updatedAt, other.updatedAt) && Objects.equals(username, other.username);
+  }
 	
 }
